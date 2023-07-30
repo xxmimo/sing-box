c27245d8eca6159941c4c955832231e55d0cd109
riolu.rs <riolu.rs@gmail.com>
2023-07-30 17:51:25 +0800

    Add vless users func

--- inbound/vless.go
+++ inbound/vless.go
@@ -114,6 +114,52 @@ func (h *VLESS) Start() error {
 	return nil
 }
 
+func (h *VLESS) AddUsers(users []option.VLESSUser) error {
+	tmp := make([]option.VLESSUser, 0, len(h.users)+len(users))
+	tmp = append(tmp, h.users...)
+	tmp = append(tmp, users...)
+	h.service.UpdateUsers(common.MapIndexed(tmp, func(index int, it option.VLESSUser) int {
+		return index
+	}), common.Map(tmp, func(it option.VLESSUser) string {
+		return it.UUID
+	}), common.Map(tmp, func(it option.VLESSUser) string {
+		return it.Flow
+	}))
+	h.users = tmp
+	return nil
+}
+
+func (h *VLESS) DelUsers(name []string) error {
+	is := make([]int, 0, len(name))
+	ulen := len(name)
+	for i := range h.users {
+		for _, u := range name {
+			if h.users[i].Name == u {
+				is = append(is, i)
+				ulen--
+			}
+			if ulen == 0 {
+				break
+			}
+		}
+	}
+	ulen = len(h.users)
+	for _, i := range is {
+		h.users[i] = h.users[ulen-1]
+		h.users[ulen-1] = option.VLESSUser{}
+		h.users = h.users[:ulen-1]
+		ulen--
+	}
+	h.service.UpdateUsers(common.MapIndexed(h.users, func(index int, it option.VLESSUser) int {
+		return index
+	}), common.Map(h.users, func(it option.VLESSUser) string {
+		return it.UUID
+	}), common.Map(h.users, func(it option.VLESSUser) string {
+		return it.Flow
+	}))
+	return nil
+}
+
 func (h *VLESS) Close() error {
 	return common.Close(
 		h.service,
